b65b3094c8d74af73f97e9a98b196befc69b9600
trevorthornton <hellotrevorthornton@gmail.com>
2014-04-02 14:37:12 -0400

    add generator for initializer

--- lib/generators/archivesspace_configuration_generator.rb
+++ lib/generators/archivesspace_configuration_generator.rb
@@ -1,6 +1,6 @@
-class ArchivesspaceApiUtilityGenerator < Rails::Generators::NamedBase
+class InitializerGenerator < Rails::Generators::NamedBase
   source_root(File.expand_path(File.dirname(__FILE__))
   def copy_initializer
-    copy_file 'archivesspace_config.rb', 'config/initializers/archivesspace_config.rb'
+    copy_file 'initializer.rb', 'config/initializers/archivesspace_config.rb'
   end
 end
