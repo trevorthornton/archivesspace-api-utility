59dd7a23c7152f4e6a856e6b4ecd375fd3f4983d
trevorthornton <hellotrevorthornton@gmail.com>
2014-04-07 17:16:06 -0400

    fix handling of multi-valued query params:

--- lib/archivesspace-api-utility.rb
+++ lib/archivesspace-api-utility.rb
@@ -15,8 +15,7 @@ module ArchivesSpaceApiUtility
     end
 
     def connect
-      uri = base_uri
-      uri.path = "/users/#{ArchivesSpaceApiUtility.configuration.username}/login"
+      uri = URI("#{base_uri}/users/#{ArchivesSpaceApiUtility.configuration.username}/login")
       response = Net::HTTP.post_form(uri, 'password' => ArchivesSpaceApiUtility.configuration.password)
       @session_token = JSON.parse(response.body)['session']
       @auth_header = { 'X-ArchivesSpace-session' => @session_token }
@@ -28,7 +27,7 @@ module ArchivesSpaceApiUtility
 
     def base_uri
       schema = ArchivesSpaceApiUtility.configuration.https ? 'https' : 'http'
-      URI("#{schema}://#{ArchivesSpaceApiUtility.configuration.host}:#{ArchivesSpaceApiUtility.configuration.port}")
+      "#{schema}://#{ArchivesSpaceApiUtility.configuration.host}:#{ArchivesSpaceApiUtility.configuration.port}"
     end
 
     def post(path,data,headers={})    
@@ -42,9 +41,27 @@ module ArchivesSpaceApiUtility
     end
 
     def get(path,params={},headers={})
-      uri = base_uri
-      uri.path = path
-      uri.query = URI.encode_www_form(params)
+      # verify that path starts with /
+      if !path.match(/^\//)
+        path = '/' + path
+      end
+
+      # NOTE: Ruby's URI module generates multi-valued query parameters in the form "key=value1&key=value2"
+      #   but the ASpace API only works if you send them like this: "key[]=value1&key[]=value2".
+      #   So ideally the next part would look like this:
+      #
+      #   uri = URI(base_uri)
+      #   uri.path = path
+      #   uri.query = params_to_query(params)
+      #
+      # ... but it doesn't. Need to see if thins changes in future versions of ASpace.
+
+      uri = "#{base_uri}#{path}"
+      if !params.empty?
+        uri += "?#{params_to_query(params)}"
+      end
+      uri = URI(uri)
+      
       request = Net::HTTP::Get.new(uri)
       headers.merge!(@auth_header)
       headers.each { |k,v| request[k] = v }
@@ -56,9 +73,42 @@ module ArchivesSpaceApiUtility
     def params_to_data(params)
       data_params = []
       params.each { |k,v| data_params << "#{k}=#{v}"}
-      data = data_params.join(' ')
+      data_params.join(' ')
     end
     
+
+    def params_to_query(params)
+      queries = []
+      
+      query_param = lambda do |k,v|
+        v = CGI.escape(v)
+        return "#{k}=#{v}"
+      end
+
+      array_to_queries = Proc.new do |k, array|
+        array.each { |v| queries << query_param.call("#{k}[]",v) }
+      end
+
+      key_value_to_queries = Proc.new do |k,v|
+        case v
+        when FalseClass
+          queries << query_param.call("#{k}",0)
+        when TrueClass
+          queries << query_param.call("#{k}",1)
+        when String
+          queries << query_param.call("#{k}",v)
+        when Array
+          array_to_queries.call(k,v)
+        when Hash
+          v.each { |kk,vv| key_value_to_queries.call("#{k}[#{kk}]",vv)}
+        end
+      end
+
+      params.each { |k,v| key_value_to_queries.call(k,v) }
+      queries.join('&')
+    end
+
   end
+
 
 end
